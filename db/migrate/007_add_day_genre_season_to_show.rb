42bccdfb7668f5b54bff908d3fb2f9a3e5f9a717
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-20 08:18:49 +0000

    Automatically backed up by Learn

---
+++ db/migrate/007_add_day_genre_season_to_show.rb
@@ -0,0 +1,9 @@
+class AddDayGenreSeasonToShow < ActiveRecord::Migration[5.1]
+  def change 
+    add_column :shows do |t|
+      t.string :day
+      t.string :genre 
+      t.string :season
+    end
+  end
+end
