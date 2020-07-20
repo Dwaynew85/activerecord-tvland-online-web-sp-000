cffede4376e03285108389a69cc0c1f1e30bd6f0
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-20 07:46:46 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,25 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    self.characters.collect { |char| "#{char.name} - #{char.show.name}"}
+  end
+
 end
+
+# ryan = Actor.new(:first_name => "Ryan", :last_name => "Reynolds")
+# deadpool = Character.new(:name => "Deadpool")
+# wade = Character.new(:name => "Wade Wilson")
+# wade.actor = ryan
+# deadpool.actor = ryan
+# origins = Show.new(:name => "X-Men Origins: Wolverine")
+# pool = Show.new(:name => "Deadpool Is Awesomesauce")
+# deadpool.show = pool
+# wade.show = origins
+# origins.save
+# deadpool.save
