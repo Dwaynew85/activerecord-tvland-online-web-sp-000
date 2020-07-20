25413a8e22585266a9f02b7144f7eb797d5e11db
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-20 08:38:08 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,6 @@ class Show < ActiveRecord::Base
   has_many :characters
   belongs_to :network
   def actors_list
-    self.characters.collect {|show| show.character.actor.full_name}
+    self.characters.collect {|show| show.actor.full_name}
   end
 end
